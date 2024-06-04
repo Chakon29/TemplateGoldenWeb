9b3eefe2ebb6e92a5739c6336c308953dee27273
Chakon29 <vi.chacon@duocuc.cl>
2024-06-04 01:13:30 -0400

    Seccionado de index.html en componentes angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+<<<<<<< Updated upstream
 
 export const routes: Routes = [];
+=======
+import { HeroComponent } from './hero/hero.component'; // Asegúrate de que esta ruta es correcta
+import { ContactComponent} from './contact/contact.component';
+
+
+export const routes: Routes = [
+  { path: 'hero', component: HeroComponent },
+  { path: 'contact', component: ContactComponent} // Define la ruta para el HeroComponent
+];
+>>>>>>> Stashed changes
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+<<<<<<< Updated upstream
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+=======
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(RouterModule.forRoot(routes))
+  ]
+})
+  .catch(err => console.error(err));
+>>>>>>> Stashed changes
